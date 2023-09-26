6f8771ccc1373d4b6607572f9075a26714886ef3
MDMSerra <122322927+MDMSerra@users.noreply.github.com>
2023-09-25 21:47:00 -0300

    tercer intento

--- src/practicagithub/entidades/Conejo.java
+++ src/practicagithub/entidades/Conejo.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicagithub.entidades;
 
-/**
- *
- * @author MAR
- */
 public class Conejo {
+    private String color;
+    private float peso;
+
+    public Conejo() {
+    }
+
+    public Conejo(String color, float peso) {
+        this.color = color;
+        this.peso = peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+    
     
 }
